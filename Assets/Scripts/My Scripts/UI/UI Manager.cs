a9b12507079e9f260f953cb24a6f4bb788feabf8
johnny <JohnnyLynd@outlook.com>
2026-05-27 23:48:19 +0800

    dialoguePane and storePanel

--- Assets/Scripts/My Scripts/UI/UI Manager.cs
+++ Assets/Scripts/My Scripts/UI/UI Manager.cs
@@ -1,4 +1,6 @@
+using System.Collections;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class UIManager : MonoBehaviour
@@ -6,8 +8,17 @@ public class UIManager : MonoBehaviour
     public static UIManager Instance {get; private set;}
     
     public GameObject interactPromptPanel;
+
+    public GameObject dialoguePanel;
     
     public TextMeshProUGUI interactText;
+
+    public TextMeshProUGUI dialogueText;
+
+    public float timebetweenDialogue = 5f;
+
+    private bool isInDialogue;
+
     
     private void Awake()
     {
@@ -35,5 +46,30 @@ public class UIManager : MonoBehaviour
     {
         interactText.text = message;
         interactPromptPanel.SetActive(true);
+    }
+
+    public void ShowDialoguePanel(string message)
+    {
+        dialogueText.text = message;
+        dialoguePanel.SetActive(true);
+    }
+
+    public void HideDialoguePanle()
+    {
+        dialoguePanel.SetActive(false);
+    }
+
+    public void StartDialogue(string message)
+    {
+        ShowDialoguePanel(message);
+
+        StartCoroutine(Dialogue());
+    }
+
+    IEnumerator Dialogue()
+    {
+        yield return new WaitForSeconds(timebetweenDialogue);
+
+        HideDialoguePanle();
     }
 }
